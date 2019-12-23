277851652acc7546d807203047b3d131a9b11972
Brodator Brominator <Brodatorbro@gmail.com>
2019-12-24 00:11:08 +0600

    Сервер

---
+++ BlueConnection/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BlueConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(444, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 119);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(444, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(413, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(22, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(525, 262);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 307);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BlueConnection/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using DarrenLee.Bluetooth;
+
+namespace BlueConnection
+{
+    public partial class Form1 : Form
+    {
+        Bluetooth_Server blueServer = new Bluetooth_Server();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Not doing anything";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "wait";
+            blueServer.Start();
+            blueServer.IsConnected += BlueServer_IsConnected;
+            blueServer.DataReceived += BlueServer_DataReceived;
+        }
+        private void BlueServer_IsConnected(object sender, EventArgs e)
+        {
+            MessageBox.Show("Connection estabiled!");
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    textBox1.Text = "connected";
+                }));
+            }
+        }
+        private void BlueServer_DataReceived(object sender, BluetoothServerEventArgs e)
+        {
+            if(InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    richTextBox1.AppendText(e.Message);
+                }));
+            }
+        }
+
+    }
+}
